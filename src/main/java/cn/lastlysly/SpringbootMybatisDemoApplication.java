ce5dcd004d6a01cb7a64cec93665240873eda66a
lastlySly <lastlySly@163.com>
2018-05-08 20:34:57 +0800

    改为可运行于外部tomcat服务器

--- src/main/java/cn/lastlysly/SpringbootMybatisDemoApplication.java
+++ src/main/java/cn/lastlysly/SpringbootMybatisDemoApplication.java
@@ -3,11 +3,22 @@ package cn.lastlysly;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootMybatisDemoApplication {
+public class SpringbootMybatisDemoApplication  extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootMybatisDemoApplication.class);
+	}
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootMybatisDemoApplication.class, args);
+		//		SpringApplication.run(SpringbootMybatisDemoApplication.class, args);
+
+
+		SpringApplication springApplication = new SpringApplication(SpringbootMybatisDemoApplication.class);
+//		springApplication.setBannerMode(Banner.Mode.OFF); 关闭banner
+		springApplication.run(args);
 	}
 }
